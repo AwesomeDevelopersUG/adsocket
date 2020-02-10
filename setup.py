6e5d8246baf2912fc87e71c99df387744d152e8b
Jakub Bokoc <jakub@awesomedevelopers.eu>
2020-02-10 16:57:20 +0100

    v0.1.0

--- setup.py
+++ setup.py
@@ -24,6 +24,10 @@ with open('adsocket/version.py', 'r') as fd:
                         fd.read(), re.MULTILINE).group(1)
 
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
 class TeamCityVersionCommand(Command):
 
     description = "Report package version to TeamCity"
@@ -60,10 +64,19 @@ setup(
     platforms='any',
     license='MIT',
     description="Websocket protocol",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     version=version,
     entry_points={
         'console_scripts': [
             'adsocket=adsocket.server:run_loop'
         ]
-    }
+    },
+    classifiers=[
+      "Programming Language :: Python :: 3.7",
+      "Programming Language :: Python :: 3.8",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+      "Development Status :: 4 - Beta"
+    ],
 )
